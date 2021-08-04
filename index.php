ff2a05e292e1649b76cca3a349141fabb70dd4a5
Martin Fors <martin.fors@frostkom.se>
2021-08-04 03:53:33 +0200

    Make extra check before looking for Base plugin

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Slideshow
 Plugin URI: https://github.com/frostkom/mf_slideshow
 Description: 
-Version: 4.8.11
+Version: 4.8.12
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -14,7 +14,7 @@ Depends: Meta Box, MF Base
 GitHub Plugin URI: frostkom/mf_slideshow
 */
 
-if(is_plugin_active("mf_base/index.php"))
+if(function_exists('is_plugin_active') && is_plugin_active("mf_base/index.php"))
 {
 	include_once("include/classes.php");
 
